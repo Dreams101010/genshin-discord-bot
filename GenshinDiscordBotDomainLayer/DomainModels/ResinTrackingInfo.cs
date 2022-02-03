0080d18539e50012ab2bf911138681613e848f8e
Alexey Gribov <m0chk0z@gmail.com>
2022-02-03 17:34:54 +0300

    Add domain entities for user and resin tracking info

---
+++ GenshinDiscordBotDomainLayer/DomainModels/ResinTrackingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinDiscordBotDomainLayer.DomainModels
+{
+    public struct ResinTrackingInfo
+    {
+        public ulong UserDiscordId { get; set; }
+        public DateTime StartTime { get; set; }
+        public int StartCount { get; set; }
+    }
+}
---
+++ GenshinDiscordBotDomainLayer/DomainModels/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinDiscordBotDomainLayer.DomainModels
+{
+    public enum UserLocale
+    {
+        enGB, ruRU
+    }
+    public struct User
+    {
+        public ulong DiscordId { get; set; }
+        public string Location { get; set; }
+        public UserLocale Locale { get; set; }
+    }
+}
