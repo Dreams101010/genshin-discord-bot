742d697f9a708b5651f9218d615b928d0228607f
Alexey Gribov <m0chk0z@gmail.com>
2022-02-03 22:44:16 +0300

    Implement UserDataModel and enum conversion helper

---
+++ GenshinDiscordBotDomainLayer/Helpers/EnumConversionHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinDiscordBotDomainLayer.DomainModels;
+
+namespace GenshinDiscordBotDomainLayer.Helpers
+{
+    public static class EnumConversionHelper
+    {
+        public static string UserLocaleToString(UserLocale locale)
+        {
+            return locale switch
+            {
+                UserLocale.ruRU => "ru-RU",
+                UserLocale.enGB => "en-GB",
+                _ => throw new ArgumentException(nameof(UserLocale))
+            };
+        }
+
+        public static UserLocale UserLocaleFromString(string str)
+        {
+            return str switch
+            {
+                "ru-RU" => UserLocale.ruRU,
+                "en-GB" => UserLocale.enGB,
+                _ => throw new ArgumentException(nameof(UserLocale))
+            };
+        }
+    }
+}
---
+++ GenshinDiscordBotSQLiteDataAccessLayer/DataModels/UserDataModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinDiscordBotDomainLayer.DomainModels;
+using GenshinDiscordBotDomainLayer.Helpers;
+
+namespace GenshinDiscordBotSQLiteDataAccessLayer.DataModels
+{
+    public struct UserDataModel
+    {
+        public ulong DiscordId { get; set; }
+        public string Locale { get; set; }
+        public string Location { get; set; }
+
+        public UserDataModel(User userFromDomain)
+        {
+            DiscordId = userFromDomain.DiscordId;
+            Locale = EnumConversionHelper.UserLocaleToString(userFromDomain.Locale);
+            Location = userFromDomain.Location;
+        }
+
+        public User ToUserDomain()
+        {
+            return new User
+            {
+                DiscordId = this.DiscordId,
+                Locale = EnumConversionHelper.UserLocaleFromString(Locale),
+                Location = this.Location
+            };
+        }
+    }
+}
