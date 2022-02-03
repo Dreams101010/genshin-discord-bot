f0031f23a7c7f333946482eb94c970fbcb363fc3
Alexey Gribov <m0chk0z@gmail.com>
2022-02-03 20:50:40 +0300

    Implement SQLite abstract command and query

---
+++ GenshinDiscordBotSQLiteDataAccessLayer/SQLiteAbstractCommand.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Serilog;
+using GenshinDiscordBotDomainLayer.Interfaces;
+
+namespace GenshinDiscordBotSQLiteDataAccessLayer
+{
+    internal abstract class SQLiteAbstractCommand<TParam, TResult> : ICommand<TParam, TResult>
+    {
+        SQLiteConnectionProvider ConnectionProvider { get; }
+        ILogger Logger { get; set; }
+
+        public SQLiteAbstractCommand(SQLiteConnectionProvider connectionProvider, 
+            ILogger loggger)
+        {
+            ConnectionProvider = connectionProvider 
+                ?? throw new ArgumentNullException(nameof(connectionProvider));
+            Logger = loggger ?? throw new ArgumentNullException(nameof(loggger));
+        }
+
+        public async Task<TResult> ExecuteAsync(TParam param, bool useTransaction = true)
+        {
+            return await RetryDecoratorAsync(param, useTransaction);
+        }
+
+        private async Task<TResult> RetryDecoratorAsync(TParam param, bool useTransaction)
+        {
+            if (useTransaction)
+            {
+                int retryCount = 0;
+                while (true)
+                {
+                    try
+                    {
+                        return await TransactionDecoratorAsync(param, useTransaction);
+                    }
+                    catch
+                    {
+                        if(retryCount < 5)
+                        {
+                            retryCount++;
+                            await Task.Delay(retryCount * 100);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return await TransactionDecoratorAsync(param, useTransaction);
+            }
+        }
+
+        private async Task<TResult> TransactionDecoratorAsync(TParam param, bool useTransaction)
+        {
+            if (useTransaction)
+            {
+                var conn = ConnectionProvider.GetConnection();
+                await conn.OpenAsync();
+                var transaction = await conn.BeginTransactionAsync();
+                try
+                {
+                    var result = await ErrorHandlingDecoratorAsync(param);
+                    await transaction.CommitAsync();
+                    return result;
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    throw;
+                }
+            }
+            else
+            {
+                return await ErrorHandlingDecoratorAsync(param);
+            }
+        }
+
+        private async Task<TResult> ErrorHandlingDecoratorAsync(TParam param)
+        {
+            try
+            {
+                return await PayloadAsync(param);
+            }
+            catch (Exception e)
+            {
+                Logger.Error($"An error has occured while executing command: {e}");
+                throw;
+            }
+        }
+
+        protected abstract Task<TResult> PayloadAsync(TParam param);
+    }
+}
---
+++ GenshinDiscordBotSQLiteDataAccessLayer/SQLiteAbstractQuery.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using GenshinDiscordBotDomainLayer.Interfaces;
+
+namespace GenshinDiscordBotSQLiteDataAccessLayer
+{
+    public abstract class SQLiteAbstractQuery<TParam, TResult> : IQuery<TParam, TResult>
+    {
+        SQLiteConnectionProvider ConnectionProvider { get; }
+        ILogger Logger { get; set; }
+
+        public SQLiteAbstractQuery(SQLiteConnectionProvider connectionProvider,
+            ILogger loggger)
+        {
+            ConnectionProvider = connectionProvider
+                ?? throw new ArgumentNullException(nameof(connectionProvider));
+            Logger = loggger ?? throw new ArgumentNullException(nameof(loggger));
+        }
+
+        public async Task<TResult> QueryAsync(TParam param, bool retry = true)
+        {
+            return await RetryDecoratorAsync(param, retry);
+        }
+
+        private async Task<TResult> RetryDecoratorAsync(TParam param, bool retry)
+        {
+            if (retry)
+            {
+                int retryCount = 0;
+                while (true)
+                {
+                    try
+                    {
+                        return await ErrorHandlingDecoratorAsync(param);
+                    }
+                    catch
+                    {
+                        if (retryCount < 5)
+                        {
+                            retryCount++;
+                            await Task.Delay(retryCount * 100);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return await ErrorHandlingDecoratorAsync(param);
+            }
+        }
+
+        private async Task<TResult> ErrorHandlingDecoratorAsync(TParam param)
+        {
+            try
+            {
+                return await PayloadAsync(param);
+            }
+            catch (Exception e)
+            {
+                Logger.Error($"An error has occured while executing query: {e}");
+                throw;
+            }
+        }
+
+        protected abstract Task<TResult> PayloadAsync(TParam param);
+    }
+}
