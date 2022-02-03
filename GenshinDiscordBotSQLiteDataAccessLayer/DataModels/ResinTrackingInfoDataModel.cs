4d44253784487c8c54c74212ab43348df36b1f92
Alexey Gribov <m0chk0z@gmail.com>
2022-02-03 23:06:42 +0300

    Implement resin tracking data model

---
+++ GenshinDiscordBotSQLiteDataAccessLayer/DataModels/ResinTrackingInfoDataModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinDiscordBotDomainLayer.DomainModels;
+
+namespace GenshinDiscordBotSQLiteDataAccessLayer.DataModels
+{
+    public struct ResinTrackingInfoDataModel
+    {
+        public ulong UserDiscordId { get; set; }
+        public string StartTime { get; set; } // stored in string form in SQLite
+        public int StartCount { get; set; }
+
+        public ResinTrackingInfoDataModel(ResinTrackingInfo resinTrackingInfo)
+        {
+            UserDiscordId = resinTrackingInfo.UserDiscordId;
+            StartTime = resinTrackingInfo.StartTime.ToString("yyyy-MM-dd HH:MM:ss.fff");
+            StartCount = resinTrackingInfo.StartCount;
+        }
+
+        public ResinTrackingInfo ToResinTrackingInfo()
+        {
+            return new ResinTrackingInfo
+            {
+                UserDiscordId = this.UserDiscordId,
+                StartTime = DateTime.Parse(this.StartTime),
+                StartCount = this.StartCount
+            };
+        }
+    }
+}
