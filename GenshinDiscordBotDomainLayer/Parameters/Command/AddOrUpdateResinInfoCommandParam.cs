fe58243465900eae6e800a1462eea000b1df6cb1
Alexey Gribov <m0chk0z@gmail.com>
2022-02-04 18:06:39 +0300

    Implement AddOrUpdateResinInfoCommand and related parameter

---
+++ GenshinDiscordBotDomainLayer/Parameters/Command/AddOrUpdateResinInfoCommandParam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinDiscordBotDomainLayer.DomainModels;
+
+namespace GenshinDiscordBotDomainLayer.Parameters.Command
+{
+    public struct AddOrUpdateResinInfoCommandParam
+    {
+        public ResinTrackingInfo ResinTrackingInfo { get; set; }
+    }
+}
---
+++ GenshinDiscordBotSQLiteDataAccessLayer/Commands/AddOrUpdateResinInfoCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinDiscordBotSQLiteDataAccessLayer.Repositories;
+using GenshinDiscordBotSQLiteDataAccessLayer.DataModels;
+using GenshinDiscordBotDomainLayer.Parameters.Command;
+using Serilog;
+
+namespace GenshinDiscordBotSQLiteDataAccessLayer.Commands
+{
+    public class AddOrUpdateResinInfoCommand : SQLiteAbstractCommand<AddOrUpdateResinInfoCommandParam, bool>
+    {
+        private ResinTrackingInfoRepository ResinRepository { get; }
+        public AddOrUpdateResinInfoCommand(
+            SQLiteConnectionProvider connectionProvider,
+            ILogger logger,
+            ResinTrackingInfoRepository resinRepository
+            ) : base(connectionProvider, logger)
+        {
+            ResinRepository = resinRepository
+                ?? throw new ArgumentNullException(nameof(resinRepository));
+        }
+
+        protected override async Task<bool> PayloadAsync(AddOrUpdateResinInfoCommandParam param)
+        {
+            var resinInfoDataModel = new ResinTrackingInfoDataModel(param.ResinTrackingInfo);
+            await ResinRepository.AddOrUpdateResinCountAsync(resinInfoDataModel);
+            return true;
+        }
+    }
+}
