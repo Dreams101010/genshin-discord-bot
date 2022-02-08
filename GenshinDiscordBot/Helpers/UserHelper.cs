31af68d2537f337c1bab8f947f087bd826332d3d
Alexey Gribov <m0chk0z@gmail.com>
2022-02-08 16:15:20 +0300

    Implement User validation helper

---
+++ GenshinDiscordBot/Helpers/UserHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinDiscordBotDomainLayer.DomainModels;
+
+namespace GenshinDiscordBotUI.Helpers
+{
+    public class UserHelper
+    {
+		public bool IsLocale(string str)
+		{
+			str = str.ToLower();
+			if (str == "ruru" || str == "engb")
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public UserLocale GetLocaleFromString(string str)
+		{
+			return str.ToLower() switch
+			{
+				"ruru" => UserLocale.ruRU,
+				"engb" => UserLocale.enGB,
+				_ => throw new ArgumentException("Invalid locale value")
+			};
+		}
+
+		public bool IsLocation(string str)
+        {
+			string[] valid = { "Not specified",
+				"Moscow, Russia", "Saint Petersburg, Russia",
+				"London, Great Britain" };
+			if (valid.Contains(str))
+            {
+				return true;
+            }
+			return false;
+        }
+	}
+}
